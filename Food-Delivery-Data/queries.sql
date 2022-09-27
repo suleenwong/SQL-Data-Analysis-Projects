0e387bd530f8acc04981e9329eccc26d8d188671
suleenwong <slwong@web.de>
2022-09-28 00:13:40 +0200

    food delivery data analysis

---
+++ Food-Delivery-Data/queries.sql
@@ -0,0 +1,112 @@
+-- Revenue per customer
+SELECT SUM(meals.meal_price*orders.order_quantity) AS revenue
+  FROM meals
+  JOIN orders ON meals.meal_id = orders.meal_id
+-- Keep only the records of customer ID 15
+WHERE user_id = 15;
+
+-- Revenue per week
+SELECT DATE_TRUNC('week', order_date) :: DATE AS delivr_week,
+       -- Calculate revenue
+       SUM(meal_price*order_quantity) AS revenue
+  FROM meals
+  JOIN orders ON meals.meal_id = orders.meal_id
+-- Keep only the records in June 2018
+WHERE DATE_TRUNC('month', order_date) = '2018-06-01'
+GROUP BY delivr_week
+ORDER BY delivr_week ASC;
+
+-- Top meals by cost
+SELECT
+  -- Calculate cost per meal ID
+  meals.meal_id,
+  SUM(meals.meal_cost*stock.stocked_quantity) AS cost
+FROM meals
+JOIN stock ON meals.meal_id = stock.meal_id
+GROUP BY meals.meal_id
+ORDER BY cost DESC
+-- Only the top 5 meal IDs by purchase cost
+LIMIT 5;
+
+-- Calculate cost per month
+SELECT
+  -- Calculate cost
+  DATE_TRUNC('month', stocking_date)::DATE AS delivr_month,
+  SUM(meals.meal_cost*stock.stocked_quantity) AS cost
+FROM meals
+JOIN stock ON meals.meal_id = stock.meal_id
+GROUP BY delivr_month
+ORDER BY delivr_month ASC;
+
+
+-- Profit per eatery
+WITH revenue AS (
+  -- Calculate revenue per eatery
+  SELECT eatery,
+         SUM(meals.meal_price*orders.order_quantity) AS revenue
+    FROM meals
+    JOIN orders ON meals.meal_id = orders.meal_id
+   GROUP BY eatery),
+
+  cost AS (
+  -- Calculate cost per eatery
+  SELECT eatery,
+         SUM(meals.meal_cost*stock.stocked_quantity) AS cost
+    FROM meals
+    JOIN stock ON meals.meal_id = stock.meal_id
+   GROUP BY eatery)
+
+   -- Calculate profit per eatery
+   SELECT revenue.eatery,
+          revenue-cost AS profit
+     FROM revenue
+     JOIN cost ON revenue.eatery = cost.eatery
+    ORDER BY profit DESC;
+
+
+-- Profit per month
+-- Set up the revenue CTE
+WITH revenue AS ( 
+	SELECT
+		DATE_TRUNC('month', order_date) :: DATE AS delivr_month,
+		SUM(meal_price*order_quantity) AS revenue
+	FROM meals
+	JOIN orders ON meals.meal_id = orders.meal_id
+	GROUP BY delivr_month),
+-- Set up the cost CTE
+  cost AS (
+ 	SELECT
+		DATE_TRUNC('month', stocking_date) :: DATE AS delivr_month,
+		SUM(meal_cost*stocked_quantity) AS cost
+	FROM meals
+    JOIN stock ON meals.meal_id = stock.meal_id
+	GROUP BY delivr_month)
+-- Calculate profit by joining the CTEs
+SELECT
+	revenue.delivr_month,
+	revenue.revenue - cost.cost AS profit
+FROM revenue
+JOIN cost ON revenue.delivr_month = cost.delivr_month
+ORDER BY revenue.delivr_month ASC;
+
+
+-- Registrations by month
+SELECT
+  -- Get the earliest (minimum) order date by user ID
+  user_id,
+  MIN(order_date) AS reg_date
+FROM orders
+GROUP BY user_id
+-- Order by user ID
+ORDER BY user_id ASC;
+
+-- Monthly active users (MAU)
+SELECT
+  -- Truncate the order date to the nearest month
+  DATE_TRUNC('month', order_date) :: DATE AS delivr_month,
+  -- Count the unique user IDs
+  COUNT(DISTINCT user_id) AS mau
+FROM orders
+GROUP BY delivr_month
+-- Order by month
+ORDER BY delivr_month ASC;
