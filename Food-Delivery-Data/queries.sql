2d167691f94057bd5d62973914629ed827c268ca
suleenwong <slwong@web.de>
2022-09-28 16:34:31 +0200

    updated

--- Food-Delivery-Data/queries.sql
+++ Food-Delivery-Data/queries.sql
@@ -91,14 +91,12 @@ ORDER BY revenue.delivr_month ASC;
 
 
 -- Registrations by month
-SELECT
-  -- Get the earliest (minimum) order date by user ID
-  user_id,
-  MIN(order_date) AS reg_date
+SELECT user_id,
+       MIN(order_date) AS reg_date
 FROM orders
 GROUP BY user_id
--- Order by user ID
 ORDER BY user_id ASC;
+
 
 -- Monthly active users (MAU)
 SELECT
@@ -110,3 +108,48 @@ FROM orders
 GROUP BY delivr_month
 -- Order by month
 ORDER BY delivr_month ASC;
+
+
+-- Running total of registrations
+WITH reg_dates AS (
+  SELECT
+    user_id,
+    MIN(order_date) AS reg_date
+  FROM orders
+  GROUP BY user_id),
+
+  regs AS (
+  SELECT
+    DATE_TRUNC('month', reg_date) :: DATE AS delivr_month,
+    COUNT(DISTINCT user_id) AS regs
+  FROM reg_dates
+  GROUP BY delivr_month)
+
+SELECT
+  -- Calculate the registrations running total by month
+  delivr_month,
+  SUM(regs) OVER (ORDER BY delivr_month ASC) AS regs_rt
+FROM regs
+-- Order by month in ascending order
+ORDER BY delivr_month ASC; 
+
+
+-- Table of MAUs and the previous month's MAU for every month
+WITH mau AS (
+  SELECT
+    DATE_TRUNC('month', order_date) :: DATE AS delivr_month,
+    COUNT(DISTINCT user_id) AS mau
+  FROM orders
+  GROUP BY delivr_month)
+
+SELECT
+  -- Select the month and the MAU
+  delivr_month,
+  mau,
+  COALESCE(
+    LAG(mau) OVER (ORDER BY delivr_month ASC),
+  0) AS last_mau
+FROM mau
+-- Order by month in ascending order
+ORDER BY delivr_month ASC;
+
